0ee9e9746042a3f4dcd7ec98713397c4d26045e1
Reinaldo Amaran <amaranreinaldo@gmail.com>
2020-02-14 20:21:39 -0300

    worksOk

--- commonElementes.py
+++ commonElementes.py
@@ -1,14 +1,20 @@
-def common_elements(Lista):
-    respuesta={}    
-    for i in range(len(Lista)):
-        list1=set(Lista[i])
-        list2=set(Lista[i-1])
-        respuesta=list1.intersection(list2)   
+"""
+Imagina que cuentas con dos listas:
+a = [1, 2, 3, 4, 5];
+b = [3, 2, 9, 3, 7];
+Realize un programa/función/clase/metodo que encuentre los elementos comúnes en
+ambas listas.
+Returns:
+    [list] -- common_elements(a, b) // [3,2]
+"""
 
-    
-    return respuesta
+def common_elements(*Lista):
+   res=list(set.intersection(*map(set,Lista)))
+   return res
 
-a=[2,3,4,6,8]
+a=[2,3,4,6,8,1]
 b=[3,8,9,1,2]
-c=[a,b]
-print(common_elements(c))     
+d=[1,20,6,2,8]
+f=[1,5,2]
+c=[a,b,d]
+print(common_elements(b,a,d))     
